90fac1d5039f549c2896530798b81cb0e71b87f3
wwwwwjddls <whwjddls859@naver.com>
2021-02-22 22:35:46 +0900

    Doit
    Doit

---
+++ Doit/do-it-example/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <hi className="title">두잇! 리액트 시작하기</hi>
+      </div>
+    )
+  }
+}
+
+export default App
